8bfdb5394cc01e04c95a4d8ac762ed4f31a87aa9
m5h6VT6 <79830624+m5h6VT6@users.noreply.github.com>
2021-10-14 00:03:23 +0100

    proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,11 @@
 import datetime
 import hashlib
 import json
-from flask inport Flask, jsonify
+from flask import Flask, jsonify
 
 
 class Blockchain:
-    
+
     def __init___(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
@@ -23,3 +23,17 @@ class Blockchain:
     
     def get_previous_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 + previous_proof**2)
+                .encode().hexdigest()
+                )
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof =+ 1
+        return new_proof
